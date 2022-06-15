b2391ddb1672dbf2e3882ed77539d9559c878cb1
ICCesar <ICCesar@users.noreply.github.com>
2022-06-14 18:27:27 -0600

    Update codecademy_project_01_friends.sql

--- codecademy_project_01_friends.sql
+++ codecademy_project_01_friends.sql
@@ -1,9 +1,11 @@
+--Create friends table
 CREATE TABLE friends (
   id INTEGER,
   name TEXT,
   birthday DATE
 );
 
+--Insert data into table
 INSERT INTO friends (id, name, birthday)
 VALUES (1, 'Ororo Munroe', '1940-05-30');
 INSERT INTO friends (id, name, birthday)
@@ -11,22 +13,28 @@ VALUES (2, 'Luke Emerson', '2000-08-15');
 INSERT INTO friends (id, name, birthday)
 VALUES (3, 'Steven Smartz', '1998-01-10');
 
+--Inspect
 SELECT *
 FROM friends;
 
+--Change Ororo to Storm
 UPDATE friends
 SET name = 'Storm'
 WHERE id = 1;
 
+--Add column
 ALTER TABLE friends
 ADD COLUMN email TEXT;
 
+--Add email to Storm
 UPDATE friends
 SET email = 'storm@codecademy.com'
 WHERE id = 1;
 
+--Delete false people
 DELETE FROM friends
 WHERE id = 1;
 
+--Review table
 SELECT *
 FROM friends;
