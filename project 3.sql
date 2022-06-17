0482ff117353d86997e1031856eaf7f26b92977d
ICCesar <ICCesar@users.noreply.github.com>
2022-06-16 20:44:20 -0600

    Create project 3.sql

---
+++ project 3.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM startups;
+
+SELECT COUNT(*)
+FROM startups;
+
+SELECT SUM(valuation)
+FROM startups;
+
+SELECT MAX(raised)
+FROM startups;
+
+SELECT MAX(raised)
+FROM startups
+WHERE stage = 'Seed';
