64ea56797cbdb8a03264e5e5c2b7e0fd6676e9ea
ICCesar <ICCesar@users.noreply.github.com>
2022-06-18 07:31:18 -0600

    Update project 3.sql

--- project 3.sql
+++ project 3.sql
@@ -25,35 +25,41 @@ WHERE stage = 'Seed';
 SELECT MIN(founded)
 FROM startups;
 
+--Let's find out the valuations among different sectors:
+
 --Average valuation by category
 SELECT category, AVG(valuation)
 FROM startups
 GROUP BY category;
 
---Average 
+--Average valuation in each category rounded to two decimal places
 SELECT category, ROUND(AVG(valuation), 2)
 FROM startups
 GROUP BY 1
 ORDER BY 2 DESC;
 
+--What are the most competitive markets?
+
+--All categories with number of companies
 SELECT category, COUNT(*)
 FROM startups
 GROUP BY category;
 
+--Categories with more than three companies by
 SELECT category, COUNT(*)
 FROM startups
 GROUP BY category
 HAVING COUNT(*) > 3
 ORDER BY 2 DESC;
 
+--Let's see if there's a difference in startups sizes among different locations:
+
+--Average size of startup by location
 SELECT location, AVG(employees)
 FROM startups
 GROUP BY location;
 
-SELECT location, AVG(employees)
-FROM startups
-GROUP BY location;
-
+--Average size of startup by location with average size > 500
 SELECT location, AVG(employees)
 FROM startups
 GROUP BY location
