ce3e7a3632d2e4ea12589da656986d27479d7226
ICCesar <ICCesar@users.noreply.github.com>
2022-06-28 07:25:50 -0600

    Update BreakIntoTech_Project_03_Customer_&_Order_Analytics.sql

--- BreakIntoTech_Project_03_Customer_&_Order_Analytics.sql
+++ BreakIntoTech_Project_03_Customer_&_Order_Analytics.sql
@@ -88,7 +88,7 @@ HAVING COUNT(orderID)>2;
 
 /*
 148 Elm St, New York City, NY 10001	      3
-515 Lincoln St, New York City, NY 10001	  3
+515 Lincoln St, New York City, NY 10001	    3
 916 Pine St, New York City, NY 10001    	3
 961 Jefferson St, New York City, NY 10001	4
 */
