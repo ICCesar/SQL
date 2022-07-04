01f1998f3ed5dea5adcf97baae64ef5494adae68
ICCesar <ICCesar@users.noreply.github.com>
2022-07-04 12:09:53 -0600

    Update BreakIntoTech_Project_05_Chinook.sql

--- BreakIntoTech_Project_05_Chinook.sql
+++ BreakIntoTech_Project_05_Chinook.sql
@@ -111,5 +111,38 @@ JOIN chinook.Genres g
 ON g.GenreId = t.GenreId;
 
 -- 13. Show the total sales made by each sales agent.
+SELECT 
+emp.FirstName, 
+emp.LastName,
+ROUND(SUM(Inv.Total), 2) AS 'TotalSales' 
+FROM chinook.Employees emp
+JOIN chinook.Customers cust 
+ON cust.SupportRepId = emp.EmployeeId
+JOIN chinook.Invoices Inv 
+ON Inv.CustomerId = cust.CustomerId
+WHERE emp.Title = 'Sales Support Agent' 
+GROUP BY emp.FirstName;
+
+/*
+FirstName LastName TotalSales
+Jane	    Peacock	 833.04
+Margaret	Park	   775.40
+Steve	    Johnson	 720.16
+*/
 
 -- 14. Which sales agent made the most in sales in 2009?
+SELECT 
+emp.FirstName, 
+emp.LastName,
+ROUND(SUM(Inv.Total), 2) AS 'TotalSales' 
+FROM chinook.Employees emp
+JOIN chinook.Customers cust 
+ON cust.SupportRepId = emp.EmployeeId
+JOIN chinook.Invoices Inv 
+ON Inv.CustomerId = cust.CustomerId
+WHERE emp.Title = 'Sales Support Agent' 
+AND Inv.InvoiceDate 
+LIKE '2009%' 
+GROUP BY emp.FirstName
+ORDER BY (round(sum(Inv.Total), 2)) DESC 
+LIMIT 1; --Steve Johnson 164.34
