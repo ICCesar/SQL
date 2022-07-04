117b3cb98cf592ead559aa47ed94233188e1e4c2
ICCesar <ICCesar@users.noreply.github.com>
2022-07-04 09:45:34 -0600

    Create BreakIntoTech_Project_05_Chinook.sql

---
+++ BreakIntoTech_Project_05_Chinook.sql
@@ -0,0 +1,31 @@
+/*
+Answers Final Project
+*/
+
+-- 01. Show Customers (their full names, customer ID, and country) who are not in the US. (Hint: != or <> can be used to say "is not equal to").
+
+-- 02. Show only the Customers from Brazil.
+
+-- 03. Find the Invoices of customers who are from Brazil. The resulting table should show the customer's full name, Invoice ID, Date of the invoice, and billing country.
+
+-- 04. Show the Employees who are Sales Agents.
+
+-- 05. Find a unique/distinct list of billing countries from the Invoice table.
+
+-- 06. Provide a query that shows the invoices associated with each sales agent. The resulting table should include the Sales Agent's full name.
+
+-- 07. Show the Invoice Total, Customer name, Country, and Sales Agent name for all invoices and customers.
+
+-- 08. How many Invoices were there in 2009?
+
+-- 09. What are the total sales for 2009?
+
+-- 10. Write a query that includes the purchased track name with each invoice line item.
+
+-- 11. Write a query that includes the purchased track name AND artist name with each invoice line item.
+
+-- 12. Provide a query that shows all the Tracks, and include the Album name, Media type, and Genre.
+
+-- 13. Show the total sales made by each sales agent.
+
+-- 14. Which sales agent made the most in sales in 2009?
